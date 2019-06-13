e360f3a41c6f13843356bd01177d1cc1237ba3b1
RahulRj09 <rahul18@navgurukul.org>
2019-06-13 10:36:26 +0530

    add. generic sort function

--- src/main/java/selectionsort/SelectionSort.java
+++ src/main/java/selectionsort/SelectionSort.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SelectionSort {
-    private static List<Integer> sort(List<Integer> elements) {
-        List<Integer> copyElements = new ArrayList<>(elements);
+    private static <T extends Comparable<T>> List<T> sort(List<T> elements) {
+        List<T> copyElements = new ArrayList<>(elements);
         for (int i = 0; i < copyElements.size() - 1; i++) {
-            int minimumNumberPosition = i;
-            for (int j = i + 1; j < copyElements.size(); j++) {
-                if (copyElements.get(j) < copyElements.get(minimumNumberPosition)) {
-                    minimumNumberPosition = j;
+            int minimumIndex = i;
+            for (int j = i +1; j < copyElements.size(); j++) {
+                if (copyElements.get(j).compareTo(copyElements.get(minimumIndex)) < 0) {
+                    minimumIndex = j;
                 }
-                int temp = copyElements.get(minimumNumberPosition);
-                copyElements.set(minimumNumberPosition, copyElements.get(i));
+                T temp = copyElements.get(minimumIndex);
+                copyElements.set(minimumIndex, copyElements.get(i));
                 copyElements.set(i, temp);
             }
         }
@@ -24,5 +24,7 @@ public class SelectionSort {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(23, 10, 14, 67, 4, -4);
         System.out.println(sort(numbers));
+        List<String> strings = Arrays.asList("b", "a", "d", "z", "c","z","b");
+        System.out.println(sort(strings));
     }
 }
