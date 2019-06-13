e49a6ac48dfa64d39c66f08d8fdbc18a2cf8b886
RahulRj09 <rahul18@navgurukul.org>
2019-06-13 11:12:55 +0530

    bug fix

--- src/main/java/selectionsort/SelectionSort.java
+++ src/main/java/selectionsort/SelectionSort.java
@@ -5,18 +5,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SelectionSort {
+
     private static <T extends Comparable<T>> List<T> sort(List<T> elements) {
         List<T> copyElements = new ArrayList<>(elements);
         for (int i = 0; i < copyElements.size() - 1; i++) {
             int minimumIndex = i;
-            for (int j = i +1; j < copyElements.size(); j++) {
+            for (int j = i + 1; j < copyElements.size(); j++) {
                 if (copyElements.get(j).compareTo(copyElements.get(minimumIndex)) < 0) {
                     minimumIndex = j;
                 }
-                T temp = copyElements.get(minimumIndex);
-                copyElements.set(minimumIndex, copyElements.get(i));
-                copyElements.set(i, temp);
             }
+            T temp = copyElements.get(minimumIndex);
+            copyElements.set(minimumIndex, copyElements.get(i));
+            copyElements.set(i, temp);
+
         }
         return copyElements;
     }
@@ -24,7 +26,7 @@ public class SelectionSort {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(23, 10, 14, 67, 4, -4);
         System.out.println(sort(numbers));
-        List<String> strings = Arrays.asList("b", "a", "d", "z", "c","z","b");
+        List<String> strings = Arrays.asList("b", "a", "d", "z", "c", "z", "b");
         System.out.println(sort(strings));
     }
 }
